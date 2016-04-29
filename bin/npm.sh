18911826c7293072941569c205f216cec785d6b9
franckysolo <franckysolo@gmail.com>
2016-04-29 20:10:28 +0200

    testing install
    Signed-off-by: franckysolo <franckysolo@gmail.com>

--- bin/npm.sh
+++ bin/npm.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-find node_modules/backbone-bootstrap-tabs -maxdepth 1 -mindepth 1 -exec mv {} . \;
-rm -rf node_modules/backbone-bootstrap-tabs
+mv backbone-bootstrap-tabs/* ../node_modules/backbone-bootstrap-tabs
